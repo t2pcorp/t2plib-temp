f0abab193b71d4adc7caacc17a821fdd7455b9b4
wattikorn <wattikorn@t2pco.com>
2021-08-10 14:21:52 +0700

    update once logic JobCheckStatus

--- src/T2PLib/Job/JobStatus.php
+++ src/T2PLib/Job/JobStatus.php
@@ -25,9 +25,18 @@ class JobStatus
 
     public function checkJobStatus()
     {
-        $this->status = $this->jobData->getLastRun()->copy()
+        if ($this->jobData->getPeriodType() == 'once')
+        {
+            $this->status = $this->jobData->getNow()->copy()
                 ->between($this->jobScheduleTime->getPreviousRun(), 
                 $this->jobScheduleTime->getPreviousRun()->copy()->addMinutes($this->jobData->getExecute()));
+        }
+        else
+        {
+            $this->status = $this->jobData->getLastRun()->copy()
+                ->between($this->jobScheduleTime->getPreviousRun(), 
+                $this->jobScheduleTime->getPreviousRun()->copy()->addMinutes($this->jobData->getExecute()));
+        }
         $this->status = $this->status? 'success' : 'fail';
     }
     
